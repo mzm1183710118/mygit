2e04168b9efb9bb165f96e28d808d3df44744002
mzm1183710118 <1044388658@qq.com>
2020-03-02 23:26:05 +0800

    计算类

---
+++ src/junit_test/calculate.java
@@ -0,0 +1,16 @@
+package junit_test;
+
+public class calculate {
+	public int add(int a,int b){
+		return a+b;
+	}
+	public int substract(int a , int b){
+		return a-b;
+	}
+	public int cheng(int a,int b){
+		return a*b;
+	}
+	public int chu(int a, int b){
+		return a/b;
+	}
+}
